270295e9ffe7106bf08579f4d2ec0e52460e0dd7
FarhanAgustiana <95676628+FarhanAgustiana@users.noreply.github.com>
2025-12-15 17:25:13 +0700

    -- dev update

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -3,7 +3,7 @@ var app = builder.Build();
 
 app.MapGet("/", () => new
 {
-    message = "Hello World from CI/CD Docker Ubuntu v12123"
+    message = "Hello World from CI/CD Docker Ubuntu v121"
 });
 
 app.Run();
